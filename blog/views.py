73da1d8fc5a60bea37a20fee6497028f693dc9b7
ElishaDallu <dalluelisha@gmail.com>
2021-07-17 11:34:44 +0530

    initial DB migration

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
+from blog.models import Post
 from djproject.settings import USE_TZ
 from django.shortcuts import render
 from django.http import request
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': Post.objects.all()
+    } 
+    return render(request, 'blog/home.html', context)
+
+
+
 
 def about(request):
-    return render(request, 'blog/about.html', {'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
