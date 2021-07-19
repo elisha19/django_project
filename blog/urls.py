ba548e9846856f08791f54286f8b577e48bd8b89
ElishaDallu <dalluelisha@gmail.com>
2021-07-19 16:54:13 +0530

    user registration

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from blog.views import home
 from django.urls import path
 from .  import views
+from users import views as user_views
+
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('register/', user_views.register, name='register'),
     path('about/', views.about, name='blog-about'),
 ]
--- djproject/urls.py
+++ djproject/urls.py
@@ -1,7 +1,6 @@
-"""djproject URL Configuration
-
+"""django_project URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +12,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls.conf import include
-import blog
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("blog.urls")),
+    path('', include('blog.urls')),
 ]
